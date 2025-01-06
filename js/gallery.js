0a4c2faedab06ea52c47276814e5bb61314ed066
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-01-06 21:45:16 +0300

    css files added

--- js/gallery.js
+++ js/gallery.js
@@ -64,17 +64,16 @@ const images = [
   },
 ];
 
-const gallery = document.querySelector(".gallery");
-
+const gallery = document.querySelector('.gallery');
 
 const galleryMap = images
   .map(
     image => `<li class="gallery-item">
-  <a class="gallery-link" href="${image.preview.slice(1,-1)}">
+  <a class="gallery-link" href="${image.preview.slice(1, -1)}">
     <img
       class="gallery-image"
-      src="${image.preview.slice(1,-1)}"
-      data-source="${image.original.slice(1,-1)}"
+      src="${image.preview.slice(1, -1)}"
+      data-source="${image.original.slice(1, -1)}"
       alt="${image.preview.description}"
     />
   </a>
