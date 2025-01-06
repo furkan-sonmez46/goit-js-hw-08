201f5ba830521ca0ce5384d7e6801eba09088d89
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-01-06 11:00:22 +0300

    map added

--- js/gallery.js
+++ js/gallery.js
@@ -64,5 +64,23 @@ const images = [
   },
 ];
 
+const gallery = document.querySelector(".gallery");
 
 
+const galleryMap = images
+  .map(
+    image => `<li class="gallery-item">
+  <a class="gallery-link" href="${image.preview.original.slice(1, -1)}">
+    <img
+      class="gallery-image"
+      src="${image.preview.slice(1, -1)}"
+      data-source="${image.preview.original.slice(1, -1)}"
+      alt="${image.preview.description}"
+    />
+  </a>
+</li>
+`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMap);
